d5f4b86b9f587652c123a9a90836a4a6c4b1602b
anchor-cable <forever.blue.moon@gmail.com>
2022-05-14 16:59:03 +0900

    feature : add template html

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,8 +1,23 @@
 use actix_web::{HttpResponse, HttpServer, get, App, ResponseError};
 use thiserror::Error;
+use askama::Template;
+
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
 
 #[derive(Error, Debug)]
-enum MyError {}
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
 
 impl ResponseError for MyError {}
 
@@ -17,6 +32,19 @@ async fn main() -> Result<(), actix_web::Error> {
 
 #[get("/")]
 async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello World!";
-    Ok(HttpResponse::Ok().body(response_body))
+    let mut entries = Vec::new();
+    entries.push(TodoEntry{
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntry{
+        id: 2,
+        text: "Second entry".to_string(),
+    });
+    
+    let html = IndexTemplate {entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
