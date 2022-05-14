c247d59e757f573933c54b822255cdc48f2b9bca
anchor-cable <forever.blue.moon@gmail.com>
2022-05-14 18:45:02 +0900

    feature : add `add_todo` handler

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,9 +1,20 @@
-use actix_web::{HttpResponse, HttpServer, get, App, ResponseError, web};
+use actix_web::{HttpResponse, HttpServer, http::header, get, post, App, ResponseError, web};
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
 use thiserror::Error;
-use askama::Template;
+use askama::Template;   
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct AddParams {
+    text : String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id : u32,
+}
 
 struct TodoEntry {
     id: u32,
@@ -43,7 +54,12 @@ async fn main() -> Result<(), actix_web::Error> {
         params![],
     ).expect("Failed to create a table `todo`.");
 
-    HttpServer::new(move || App::new().service(index).data(pool.clone()))
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(add_todo)
+            .data(pool.clone())
+        })
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
@@ -72,4 +88,17 @@ async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpRespo
     Ok(HttpResponse::Ok()
         .content_type("text/html")
         .body(response_body))
+}
+
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    println!("request has come!");
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
 }
